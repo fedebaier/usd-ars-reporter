aca87bdf71272b52b338518978c4d6686186511f
Federico Baier <fedebaier@gmail.com>
2024-05-30 18:50:13 -0300

    Handle criptoya api errors

--- index.js
+++ index.js
@@ -9,54 +9,58 @@ if (!BOT_TOKEN || !CHAT_ID) {
   throw new Error('Please configure environment variables!');
 }
 
-const [dolar, buenbit, lemon, belo, letsbit] = await Promise.all([
-  axios.get('https://criptoya.com/api/dolar').then((r) => r.data),
-  axios.get('https://criptoya.com/api/buenbit/usdt/ars/0.1').then((r) => r.data),
-  axios.get('https://criptoya.com/api/lemoncash/usdt/ars/0.1').then((r) => r.data),
-  axios.get('https://criptoya.com/api/belo/usdt/ars/0.1').then((r) => r.data),
-  axios.get('https://criptoya.com/api/letsbit/usdt/ars/0.1').then((r) => r.data),
-]);
-
-const date = new Date();
-
-const message = `Cotizaciones al <b>${date.toLocaleString('es-ES', {
-  timeZone: 'America/Buenos_Aires',
-})}</b>
-
-🤑 <b>USDT - BuenBit</b> 🤑
-Compra: ${buenbit.ask}
-Venta: ${buenbit.bid}
-
-🤑 <b>USDT - Lemon</b> 🤑
-Compra: ${lemon.ask}
-Venta: ${lemon.bid}
-
-🤑 <b>USDT - Belo</b> 🤑
-Compra: ${belo.ask}
-Venta: ${belo.bid}
-
-🤑 <b>USDT - Let'sBit</b> 🤑
-Compra: ${letsbit.ask}
-Venta: ${letsbit.bid}
-
-💵 <b>BLUE</b> 💵
-Compra: ${dolar.blue.ask}
-Venta: ${dolar.blue.bid}
-
-💵 <b>MEP</b> 💵
-AL30: ${dolar.mep['al30']['24hs'].price}
-GD30: ${dolar.mep['gd30']['24hs'].price}
-
-💵 <b>CCL</b> 💵
-AL30: ${dolar.ccl['al30']['24hs'].price}
-GD30: ${dolar.ccl['gd30']['24hs'].price}
-
-💵 <b>TARJETA</b> 💵
-${dolar.tarjeta.price}
-
-💵 <b>OFICIAL</b> 💵
-${dolar.oficial.price}`;
-
 const bot = new Bot(BOT_TOKEN);
+const date = new Date();
+let message = '';
+
+try {
+  const [dolar, buenbit, lemon, belo, letsbit] = await Promise.all([
+    axios.get('https://criptoya.com/api/dolar').then((r) => r.data),
+    axios.get('https://criptoya.com/api/buenbit/usdt/ars/0.1').then((r) => r.data),
+    axios.get('https://criptoya.com/api/lemoncash/usdt/ars/0.1').then((r) => r.data),
+    axios.get('https://criptoya.com/api/belo/usdt/ars/0.1').then((r) => r.data),
+    axios.get('https://criptoya.com/api/letsbit/usdt/ars/0.1').then((r) => r.data),
+  ]);
+
+  message = `Cotizaciones al <b>${date.toLocaleString('es-ES', {
+    timeZone: 'America/Buenos_Aires',
+  })}</b>
+
+  🤑 <b>USDT - BuenBit</b> 🤑
+  Compra: ${buenbit.ask}
+  Venta: ${buenbit.bid}
+
+  🤑 <b>USDT - Lemon</b> 🤑
+  Compra: ${lemon.ask}
+  Venta: ${lemon.bid}
+
+  🤑 <b>USDT - Belo</b> 🤑
+  Compra: ${belo.ask}
+  Venta: ${belo.bid}
+
+  🤑 <b>USDT - Let'sBit</b> 🤑
+  Compra: ${letsbit.ask}
+  Venta: ${letsbit.bid}
+
+  💵 <b>BLUE</b> 💵
+  Compra: ${dolar.blue.ask}
+  Venta: ${dolar.blue.bid}
+
+  💵 <b>MEP</b> 💵
+  AL30: ${dolar.mep['al30']['24hs'].price}
+  GD30: ${dolar.mep['gd30']['24hs'].price}
+
+  💵 <b>CCL</b> 💵
+  AL30: ${dolar.ccl['al30']['24hs'].price}
+  GD30: ${dolar.ccl['gd30']['24hs'].price}
+
+  💵 <b>TARJETA</b> 💵
+  ${dolar.tarjeta.price}
+
+  💵 <b>OFICIAL</b> 💵
+  ${dolar.oficial.price}`;
+} catch (error) {
+  message = `<b>Error</b>: ${JSON.stringify(error)}`;
+}
 
 await bot.api.sendMessage(CHAT_ID, message, { parse_mode: 'HTML' });
